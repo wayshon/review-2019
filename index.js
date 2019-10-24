fbf59edbf49abd41cf27aaca43507c7771d92e9d
wayshon <wayshon@qq.com>
2019-10-24 22:40:58 +0800

    动态规划求最大子数组（不连续的子项）

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // 就是找一个数组中拥有最大和的子数组，返回其和
-
-// 子数组长度为 1 到 list.length
-// list元素逐个进来，计算每种length情况下的最大值
-// 当所有list元素都处理后，就能知道最后一行所有length下的值，选取最大的
-const arr = [-1, 4, -2];
+ /**
+ * 子数组长度为 1 到 list.length
+ * list元素逐个进来，计算每种length情况下的最大值
+ * 当所有list元素都处理后，就能知道最后一行所有length下的值，选取最大的
+ */
+const arr = [-1, 4, -2, 1];
 function fn(list) {
     const table = [];
     for (let i = 0; i < list.length; i++) {
@@ -13,9 +14,20 @@ function fn(list) {
                 table[i][tempLength] = list[i];
                 continue;
             }
+            if(tempLength === 0) {
+                table[i][tempLength] = Math.max(list[i], table[i - 1][tempLength]);
+            } else {
+                table[i][tempLength] = Math.max(list[i] + table[i - 1][tempLength - 1], table[i - 1][tempLength]);
+            }
         }
     }
-    console.log(table)
+    console.log(table);
+    const lasts = table[table.length - 1];
+    let max = lasts[lasts.length - 1];
+    for (let i = lasts.length - 2; i >= 0; i--) {
+        max = Math.max(lasts[i], max);
+    }
+    return max;
 }
 
-fn(arr)
+console.log(fn(arr))
