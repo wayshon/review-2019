5dd7bf3f5710e49abe5db0eb0075bd310b0699d6
wayshon <wayshon@qq.com>
2019-10-24 14:34:26 +0800

    求最大子数组 动态规划未完成

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-var arr = [0,1,2,1,0,2]
+// 就是找一个数组中拥有最大和的子数组，返回其和
 
-// 冒泡
-// for (let i = 0; i < arr.length - 1; i++) {
-//     for (let j = i + 1; j < arr.length; j++) {
-//         if (arr[i] > arr[j]) {
-//             let t = arr[i];
-//             arr[i] = arr[j];
-//             arr[j] = t;
-//         }
-//     }
-// }
+// 子数组长度为 1 到 list.length
+// list元素逐个进来，计算每种length情况下的最大值
+// 当所有list元素都处理后，就能知道最后一行所有length下的值，选取最大的
+const arr = [-1, 4, -2];
+function fn(list) {
+    const table = [];
+    for (let i = 0; i < list.length; i++) {
+        table[i] = [];
+        for (let tempLength = 0; tempLength < list.length; tempLength++) {
+            if (i === 0) {
+                table[i][tempLength] = list[i];
+                continue;
+            }
+        }
+    }
+    console.log(table)
+}
 
-// 插入
-// let j;
-// for (let i = 1; i < arr.length; i++) {
-//     let temp = arr[i];
-//     for (j = i - 1; temp < arr[j]; j--) {
-//         arr[j + 1] = arr[j];
-//     }
-//     console.log(j)
-//     arr[j] = temp;
-// }
-
-
-
-console.log(arr);
+fn(arr)
