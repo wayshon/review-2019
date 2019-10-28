3390804bb3541bf21ad7843e90c3b26a76f9867f
wayshon <wayshon@qq.com>
2019-10-28 23:54:24 +0800

    ing

--- index.js
+++ index.js
@@ -1,55 +1,18 @@
-// 就是找一个数组中拥有最大和的子数组，返回其和
- /**
- * 子数组长度为 1 到 list.length
- * list元素逐个进来，计算每种length情况下的最大值
- * 当所有list元素都处理后，就能知道最后一行所有length下的值，选取最大的
- */
-const arr = [-1, 4, 2, -1, 2, -1];
-
-// function fn(list) {
-//     const table = [];
-//     for (let i = 0; i < list.length; i++) {
-//         table[i] = [];
-//         for (let tempLength = 0; tempLength < list.length; tempLength++) {
-//             if (i === 0) {
-//                 table[i][tempLength] = list[i];
-//                 continue;
-//             }
-//             if(tempLength === 0) {
-//                 table[i][tempLength] = Math.max(list[i], table[i - 1][tempLength]);
-//             } else {
-//                 table[i][tempLength] = Math.max(list[i] + table[i - 1][tempLength - 1], table[i - 1][tempLength]);
-//             }
-//         }
-//     }
-//     console.log(table);
-//     const lasts = table[table.length - 1];
-//     let max = lasts[lasts.length - 1];
-//     for (let i = lasts.length - 2; i >= 0; i--) {
-//         max = Math.max(lasts[i], max);
-//     }
-//     return max;
-// }
-
-function fu(list) {
-    let max = list[0];
-    let index = 0;
-    let length = 1;
-    let len = 1;
-    while (len <= list.length) {
-        for (let i = len; i <= list.length; i++) {
-            let sum = list.slice(i - len, len).reduce((a,b) => a+b, 0);
-            if (sum > max) {
-                max = sum;
-                index = i;
-                length = len;
-            }
-        }
-        len++;
-    }
-    return list.slice(index - length, length);
+function A() {
+    this.a = 11
 }
 
-console.log(fu(arr))
+A.prototype.aa = 22;
+
+function B() {
+    this.b = 33
+}
+
+B.prototype.bb = 44;
 
 
+var c = new A();
+c.__proto__ = B.prototype;
+console.log(c.__proto__);
+console.log(c.__proto__ === A.prototype);
+console.log(c.__proto__ === B.prototype);
