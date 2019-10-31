f087135427a74a4de87cb0f210abcc7b05dd4788
wayshon <wayshon@qq.com>
2019-10-31 22:38:49 +0800

    没写完 join array

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-class P {
-    constructor() {
-        this.age = 1;
-        return {
-            age: 6
+function innerjoin(list1, list2) {
+    let short, long;
+    if (list1.length > list2.length) {
+        long = [...list1];
+        short = [...list2];
+    } else {
+        long = [...list2];
+        short = [...list1];
+    }
+
+    for (let i = 0; i < short.length; i++) {
+        const temp = short[i];
+        for (let j = 0; j < long.length; j++) {
+            if (temp === )
         }
     }
 }
 
-let p = new P()
-console.log(p)
+const a = [1, 1];
+const b = [1];
+const result = innerjoin(a, b);
+console.log(result);
