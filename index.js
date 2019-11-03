737aa53bd8d19fad433e56a242a931e9c893482c
wayshon <wayshon@qq.com>
2019-11-04 00:53:19 +0800

    ing

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-function innerjoin(list1, list2) {
-    let short, long;
-    if (list1.length > list2.length) {
-        long = [...list1];
-        short = [...list2];
-    } else {
-        long = [...list2];
-        short = [...list1];
-    }
+// 给定 nums = [2, 7, 11, 15, 4, 8, 0, 5, 9], target = 9
+// 找出arr中所有的和为target的元素子集
+// nums[0] + nums[1] = 2 + 7 = 9
+// [ [ 2, 7 ], [ 4, 5 ], [ 0, 9 ] ]
 
-    for (let i = 0; i < short.length; i++) {
-        const temp = short[i];
-        for (let j = 0; j < long.length; j++) {
-            if (temp === )
-        }
-    }
+const nums = [2, 7, 11, 15, 4, 8, 0, 5, 9], target = 9;
+
+function calc(list, t) {
+    const results = [];
+    
+    return results;
 }
 
-const a = [1, 1];
-const b = [1];
-const result = innerjoin(a, b);
-console.log(result);
+console.log(calc(nums, target))
--- pedding.md
+++ pedding.md
@@ -1,12 +1,10 @@
 ## 未完成
 
-### 自己的博客与笔记
+### 总结一下垃圾分类的亮点
 
 ### 总结一下自己封装的react
 
-### 遇到的难点
-- position fixed 会导致滚动穿透，父容器也fixed会导致滚动高度没了
-- transform会将 fixed 变成 absolute
+### 总结一下即刻出行的亮点
 
 #### 复习进度
 - 算法
@@ -19,3 +17,8 @@
 - 三色gc
 - vue 响应式原理
 - 整个react
+
+### 遇到的难点
+- position fixed 会导致滚动穿透，父容器也fixed会导致滚动高度没了
+- transform会将 fixed 变成 absolute
+
